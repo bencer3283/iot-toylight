55b76252000c3e0452d61e09d45cae5ee19a3c52
Po-Sheng Cheng <bencer@outlook.com>
2024-05-03 18:56:44 -0400

    app.js implemented

--- server/app.js
+++ server/app.js
@@ -9,18 +9,27 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-let reset = false;
+let is_light_one_on = false;
+let is_light_two_on = false;
 
 app.post('/', (req, res) => {
     console.log('request: POST ' + JSON.stringify(req.body))
-    res.send(reset);
+    if(req.body.id == 1){
+      is_light_one_on = req.body.is_on
+      if(is_light_two_on){
+        res.send(true)
+      }
+      else res.send(false)
+    }
+    else if(req.body.id == 2){
+      is_light_two_on = req.body.is_on
+      if(is_light_one_on){
+        res.send(true)
+      }
+      else res.send(false)
+    }s
 })
 
-app.get('/reset', (req, res) => {
-    reset = !reset;
-    console.log("reset: " + reset)
-    res.send(reset)
-})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
